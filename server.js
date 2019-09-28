8b3e418a55ce21e750293ad26169ec17300cf31d
rohinsha <rohinshag@gamenous.com>
2019-09-28 16:28:45 +0530

    add corsoptions

--- server.js
+++ server.js
@@ -2,13 +2,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+var cors = require('cors');
+app.use(cors())
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
 });
+var corsOptions = {
+    origin: 'http://api.tvmaze.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/programScheduleTask'));
-app.get('/*', function(req,res) {
+app.get('/*',cors(corsOptions), function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/programScheduleTask/index.html'));
 });
 // Start the app by listening on the default Heroku port
