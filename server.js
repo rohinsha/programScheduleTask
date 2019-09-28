c5d9a8dac70ba47ebcab1619cf1b31d696de7e4a
rohinsha <rohinshag@gamenous.com>
2019-09-28 16:32:48 +0530

    Add corsoptions for all routes

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const path = require('path');
 const app = express();
 var cors = require('cors');
 app.use(cors())
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+// });
 var corsOptions = {
-    origin: 'http://api.tvmaze.com',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
   }
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/programScheduleTask'));
