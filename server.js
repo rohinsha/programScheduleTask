aca0595ccf358ec3d3c222c900553ee795b063aa
rohinsha <rohinshag@gamenous.com>
2019-09-28 16:43:57 +0530

    Add corsoptions for app.use

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 var cors = require('cors');
-app.use(cors())
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     next();
-// });
+app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 var corsOptions = {
     "origin": "*",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
@@ -17,7 +20,8 @@ var corsOptions = {
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/programScheduleTask'));
 app.get('/*',cors(corsOptions), function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/programScheduleTask/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/programScheduleTask/index.html'));
 });
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
